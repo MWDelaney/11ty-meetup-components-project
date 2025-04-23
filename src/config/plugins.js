23f2e3771d2318e74c95b96f16c0687a8a26052f
mwdelaney <michael@michaeldelaney.me>
2025-04-23 13:45:47 -0400

    🚧 Work from the meetup

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -4,7 +4,6 @@
 */
 
 import metagenPlugin from 'eleventy-plugin-metagen';
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { EleventyRenderPlugin } from "@11ty/eleventy";
 
 export default {
@@ -15,26 +14,6 @@ export default {
   metagen: async function (eleventyConfig) {
     // Add plugin to eleventyConfig
     eleventyConfig.addPlugin(metagenPlugin);
-  },
-
-  /**
-   * Eleventy Image plugin
-   * https://www.11ty.dev/docs/plugins/image/
-   */
-  image: async function (eleventyConfig) {
-    // Add plugin to eleventyConfig
-    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-      outputDir: "./public/assets/images",
-      urlPath: "/assets/images/",
-      extensions: "html",
-      formats: ["auto"],
-
-      // Attributes assigned on <img> override these values.
-      defaultAttributes: {
-        loading: "lazy",
-        decoding: "async",
-      },
-    });
   },
 
   /**
